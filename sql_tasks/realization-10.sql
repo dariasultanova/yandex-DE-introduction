7a007bb4bf3f27b3e961b401a0fed692c9b8c6d0
Daria Sultanova <dariasltnv@gmail.com>
2022-10-05 18:23:59 +0300

    Delete sql_tasks directory

--- sql_tasks/realization-10.sql
+++
@@ -1,7 +0,0 @@
--- обновите представление и снова запросите данные за май 2021
-REFRESH MATERIALIZED VIEW client_activity;
-
-SELECT *
-FROM client_activity
-WHERE EXTRACT('month' from  invoice_month) = 5 
-and EXTRACT('year' from invoice_month)=2021;
--- sql_tasks/realization-14.sql
+++
@@ -1,8 +0,0 @@
--- этот файл не сдаётся, но вы можете использовать его для своего кода
-ALTER TABLE "002_DM_clients"
-ADD age INTEGER;
-
-UPDATE "002_DM_clients"
-SET age = "002_BUFF_clients".age
-FROM  "002_BUFF_clients"
-WHERE "002_DM_clients".client_id = "002_BUFF_clients".client_id
